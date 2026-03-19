011421ff2f34103d32122cbe8121da465110a651
Lorenzo <calderonlorenzo026@gmail.com>
2026-03-19 19:16:47 +0000

    added a46 code

--- main.cpp
+++ main.cpp
@@ -9,11 +9,27 @@ int main()
     double factor = 0.1;
     double scholarship = 10000.0;
 
-    // Use the following input format: <status> <gender> <major>
-    // student female cs ; or student male math ; or nonstudent male cs ; etc.
     cin >> status >> gender >> major;
 
-    // TODO: Determine factor based on status, gender, and major using if statements
+    if (status == "student") {
+        factor = 0.2;
+        if (gender == "female") {
+            factor = 1.0;                    
+            if (major == "cs") {
+                factor = 3.0;                
+            }
+        } else {
+            if (major == "cs") {
+                factor = 2.0;                
+            }
+            
+        }
+    } else {                                 
+        if (major == "cs") {
+            factor = 0.5;                    
+        
+        }
+    }
 
     double finalScholarship = scholarship * factor;
 
